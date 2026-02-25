2a92a6a3ee8be4d23984fd80c58779f38ae3a7e7
kuljeet choure <kuljeetchoure45@gmail.com>
2026-02-25 18:45:35 +0530

    Refactor googleAuth for user creation and token handling

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,32 +1,37 @@
-export const googleAuth = async (req, res) => {
-  try {
-    console.log("BODY:", req.body)
+import UserModel from "../models/user.model.js"
+import { getToken } from "../utils/token.js"
 
-    const { name, email } = req.body
 
-    if (!name || !email) {
-      return res.status(400).json({ message: "Name or Email missing" })
+export const googleAuth = async (req,res) => {
+    try {
+        
+        const {name , email} = req.body
+        let user = await UserModel.findOne({email})
+        if(!user){
+            user = await UserModel.create({
+                name , email
+            })
+        }
+        let token = await getToken(user._id)
+        res.cookie("token" , token , {
+            httpOnly:true,
+            secure:true,
+            sameSite:"none",
+            maxAge:7 * 24 * 60 * 60 * 1000
+
+        })
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({message:`googleSignup Error  ${error}`})
     }
+    
+}
 
-    let user = await UserModel.findOne({ email })
-
-    if (!user) {
-      user = await UserModel.create({ name, email })
+export const logOut = async (req,res) => {
+    try {
+        await res.clearCookie("token")
+         return res.status(200).json({message:"LogOut Successfully"})
+    } catch (error) {
+        return res.status(500).json({message:`Logout Error  ${error}`})
     }
-
-    const token = await getToken(user._id)
-
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      path: "/"
-    })
-
-    return res.status(200).json(user)
-
-  } catch (error) {
-    console.log("GOOGLE AUTH ERROR:", error)
-    return res.status(500).json({ message: error.message })
-  }
 }
