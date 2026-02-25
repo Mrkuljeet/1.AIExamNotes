45db7105110c6caeda1a394d8285261c7a2cf21c
kuljeet choure <kuljeetchoure45@gmail.com>
2026-02-25 17:43:17 +0530

    Update CORS settings to a single origin
    Simplified CORS configuration to allow only one origin.

--- server/index.js
+++ server/index.js
@@ -25,17 +25,10 @@ app.post(
 
 
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://one-aiexamnotesclient.onrender.com"
-    ],
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: "https://one-aiexamnotesclient.onrender.com",
+  credentials: true,
+}));
 
 
 app.use(express.json())
