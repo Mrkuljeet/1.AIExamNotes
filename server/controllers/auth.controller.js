a6561aa2377dea9e64b41fbc5e3b680e1cc32fdc
kuljeet choure <kuljeetchoure45@gmail.com>
2026-02-25 18:52:13 +0530

    Enhance googleAuth with input validation
    Added validation for name and email in googleAuth function.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,37 +1,52 @@
 import UserModel from "../models/user.model.js"
 import { getToken } from "../utils/token.js"
 
+export const googleAuth = async (req, res) => {
+  try {
+    console.log("BODY:", req.body)
 
-export const googleAuth = async (req,res) => {
-    try {
-        
-        const {name , email} = req.body
-        let user = await UserModel.findOne({email})
-        if(!user){
-            user = await UserModel.create({
-                name , email
-            })
-        }
-        let token = await getToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:true,
-            sameSite:"none",
-            maxAge:7 * 24 * 60 * 60 * 1000
+    const { name, email } = req.body
 
-        })
-        return res.status(200).json(user)
-    } catch (error) {
-        return res.status(500).json({message:`googleSignup Error  ${error}`})
+    if (!name || !email) {
+      return res.status(400).json({ message: "Name or Email missing" })
     }
-    
+
+    let user = await UserModel.findOne({ email })
+
+    if (!user) {
+      user = await UserModel.create({ name, email })
+    }
+
+    const token = await getToken(user._id)
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/"
+    })
+
+    return res.status(200).json(user)
+
+  } catch (error) {
+    console.log("GOOGLE AUTH ERROR:", error)
+    return res.status(500).json({ message: error.message })
+  }
 }
 
-export const logOut = async (req,res) => {
-    try {
-        await res.clearCookie("token")
-         return res.status(200).json({message:"LogOut Successfully"})
-    } catch (error) {
-        return res.status(500).json({message:`Logout Error  ${error}`})
-    }
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: "/"
+    })
+
+    return res.status(200).json({ message: "Logout Successfully" })
+
+  } catch (error) {
+    console.log("LOGOUT ERROR:", error)
+    return res.status(500).json({ message: error.message })
+  }
 }
